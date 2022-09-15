cf1c8eedca695bd1a5080430d9e04514002cefcd
MBearo <wuweiqi@baidu.com>
2022-09-15 19:31:06 +0800

    update

--- 02/src/panda/main.go
+++ 02/src/panda/main.go
@@ -2,12 +2,23 @@ package main
 
 import "fmt"
 
-var keys = map[string]int{
-	"a": 1,
-	"b": 2,
+type Bar struct {
+	value int
+}
+type Foo struct {
+	bar   Bar
+	value int
+}
+
+func trans(foo Foo) {
+	foo.bar.value = 10
 }
 
 func main() {
-	lll, xxx := keys["a"]
-	fmt.Printf("xxx %v %v \n", lll, xxx)
+	f := Foo{Bar{1}, 2}
+	fmt.Println(f.value)
+	fmt.Println(f.bar.value)
+	trans(f)
+	fmt.Println(f.value)
+	fmt.Println(f.bar.value)
 }
